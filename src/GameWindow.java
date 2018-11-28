b412adc89d8dedd403d614865722ae50aa3f3218
Tchovan <Tchovan@luc.edu>
2018-11-28 12:47:57 -0600

    Added images to JButtons. (Need to randomize and fix sizes)

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -3,208 +3,226 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.net.*;
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
-
 import javax.imageio.ImageIO;
 import javax.swing.*;
 
 
+// images for game are downloaded during runtime. Program runs faster when images are downloaded first and saved as a file
+// i.e:
+// File f = new file("filepath")
+// bimg =  ImageIO.read(f);
+
+
 public class GameWindow extends JFrame {        //contains the in-game puzzle board.
-	JButton[][] board;
-	Container pane;
-	MenuBar mb = new MenuBar();
-	ArrayList<Integer> reset; //keeps track of button order for board reset
-	Font font = new Font("sans-serif", Font.BOLD, 100);
+    JButton[][] board;
+    Container pane;
+    MenuBar mb = new MenuBar();
+    ArrayList<Integer> reset; //keeps track of button order for board reset
+    Font font = new Font("sans-serif", Font.BOLD, 100);
 
-	private Point btnPoint;
-	private Point nullBtnPoint;
-	private Point one = new Point(0,0);
-	private Point two = new Point(0,1);
-	private Point three = new Point(0,2);
-	private Point four = new Point(1,0);
-	private Point five = new Point(1,1);
-	private Point six = new Point(1,2);
-	private Point seven = new Point(2,0);
-	private Point eight = new Point(2,1);
-	private Point nine = new Point(2,2);
+    private Point btnPoint;
+    private Point nullBtnPoint;
+    private Point one = new Point(0,0);
+    private Point two = new Point(0,1);
+    private Point three = new Point(0,2);
+    private Point four = new Point(1,0);
+    private Point five = new Point(1,1);
+    private Point six = new Point(1,2);
+    private Point seven = new Point(2,0);
+    private Point eight = new Point(2,1);
+    private Point nine = new Point(2,2);
 
-	public GameWindow() {
-		super();
+    public GameWindow() {
+        super();
 
-		display();
-		buttonSwap();
-	}
+        display();
+        buttonSwap();
+    }
 
-	private void display() {
-		pane = getContentPane();
-		pane.setLayout(new GridLayout(3, 3));
-		setTitle("Sliding Puzzle");
-		setSize(1100, 1100);
-		setResizable(false);
-		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-		board = new JButton[3][3];
-		setJMenuBar(mb.menuBar);
-	}
+    private void display() {
+        pane = getContentPane();
+        pane.setLayout(new GridLayout(3, 3));
+        setTitle("Sliding Puzzle");
+        setSize(1100, 1100);
+        setResizable(false);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        board = new JButton[3][3];
+        setJMenuBar(mb.menuBar);
+    }
 
-	public void buttonSwap() {
-		int maxNum = 9;
-		Integer currentNum = 0; //temp holds random number
+    public void buttonSwap() {
+        int maxNum = 9;
+        Integer currentNum = 0; //temp holds random number
 
-		reset = new ArrayList<Integer>(); //keeps track of button order for board reset
-		ArrayList<Integer> numbers = new ArrayList<Integer>(); //holds button values
-		numbers.add(1);
-		numbers.add(2);
-		numbers.add(3);
-		numbers.add(4);
-		numbers.add(5);
-		numbers.add(6);
-		numbers.add(7);
-		numbers.add(8);
-		numbers.add(null);
+        reset = new ArrayList<Integer>(); //keeps track of button order for board reset
+        ArrayList<Integer> numbers = new ArrayList<Integer>(); //holds button values
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+        numbers.add(6);
+        numbers.add(7);
+        numbers.add(8);
+        numbers.add(null);
+        int num = 10;
 
-		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 3; j++) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
 
-				JButton btn = new JButton();
+                JButton btn = new JButton();
 
-				Random rand = new Random();
-				int n = rand.nextInt(maxNum) + 0; //7 is max, 0 is min
-				currentNum = numbers.get(n);
-				reset.add(currentNum); //keeps track of button order for board reset
-				numbers.remove(n);
-				maxNum--;
+                Random rand = new Random();
+                int n = rand.nextInt(maxNum) + 0; //7 is max, 0 is min
+                currentNum = numbers.get(n);
+                reset.add(currentNum); //keeps track of button order for board reset
+                numbers.remove(n);
+                maxNum--;
 
-				BufferedImage[][] ImageArr = new BufferedImage[3][3];
-				ImageArr = imageDivide(ImageArr);
+                BufferedImage[][] ImageArr = new BufferedImage[3][3];
+                ImageArr = imageDivide(ImageArr);
 
-				ImageIcon img = new ImageIcon(ImageArr[i][j]);
-				btn.setIcon(getScaledImage(img));
+                ImageIcon img = new ImageIcon(ImageArr[i][j]);
 
-				btn.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 100));
-				if (currentNum == null) { //special format for blank tile
-					btn.setBackground(Color.black);
-					btn.setText("");
-					btn.setIcon(null);
+                btn.setIcon(getScaledImage(img));
 
-				} else {
-					btn.setBackground(Color.white);
-					btn.setText(String.valueOf(currentNum));
-				}
-				btn.addActionListener(new ActionListener() {
-					@Override
-					public void actionPerformed(ActionEvent e) {    //swaps the button clicked on with the null button
-						JButton nullBtn = new JButton();
-						nullBtn = getNullButton(board);
+                btn.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 100));
+                if (currentNum == null) { //special format for blank tile
+                    btn.setBackground(Color.black);
+                    btn.setText("");
+                    btn.setIcon(null);
 
-						if (checkNextTo(btn, nullBtn, board)) {    //if the button clicked on is vertically or horizontally next to the null button
-							nullBtn.setBackground(btn.getBackground());        //set null button to the button clicked on
-							nullBtn.setText(btn.getText());
-							nullBtn.setIcon(btn.getIcon());
+                } else {
+                    btn.setBackground(Color.white);
+                    btn.setText(String.valueOf(currentNum));
+                }
+                btn.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {    //swaps the button clicked on with the null button
+                        JButton nullBtn = new JButton();
+                        nullBtn = getNullButton(board);
 
-							btn.setBackground(Color.black);        //sets the button clicked on to the new null button
-							btn.setText("");
-							btn.setIcon(null);
-						} else {
-							System.out.println("You can't swap these tiles.");
-						}
-					}
-				});
+                        if (checkNextTo(btn, nullBtn, board)) {    //if the button clicked on is vertically or horizontally next to the null button
+                            nullBtn.setBackground(btn.getBackground());        //set null button to the button clicked on
+                            nullBtn.setText(btn.getText());
+                            nullBtn.setIcon(btn.getIcon());
 
-				board[i][j] = btn;
-				pane.add(btn);
-			}
-		}
-	}
+                            btn.setBackground(Color.black);        //sets the button clicked on to the new null button
+                            btn.setText("");
+                            btn.setIcon(null);
+                        } else {
+                            System.out.println("You can't swap these tiles.");
+                        }
+                    }
+                });
 
-	public BufferedImage[][] imageDivide(BufferedImage[][] imgs) {      //crops photo into nine parts
-		BufferedImage[][] images = new BufferedImage[3][3];
+                board[i][j] = btn;
+                pane.add(btn);
+                System.out.println("Loading................................ "  + num + "%");
+                num = num + 10;
+            }
+        }
+        System.out.println("Loading................................ 100%");
+    }
 
-		int x = 1;
-		int y = 1;
-		int subW = 1;
-		int subH = 1;
-		//int rW = 1;
-		//int rH = 1;
-		BufferedImage bimg = null;
-		URL url = null;
+    public BufferedImage[][] imageDivide(BufferedImage[][] imgs) {      //crops photo into nine parts
+        BufferedImage[][] images = new BufferedImage[3][3];
 
-		try {
-			url = new URL("http://www.papertraildesign.com/wp-content/uploads/2017/06/emoji-sunglasses.png");       //works for any image, just change URL path
-			bimg =  ImageIO.read(url);
-			subW = ImageIO.read(url).getWidth() / 3;
-			//rW = ImageIO.read(f).getWidth() % 3 * 3;
-			subH = ImageIO.read(url).getHeight() / 3;
-			//rH = ImageIO.read(f).getWidth() % 3 * 3;
+        //initializations required prior to try-catch
+        int x = 1;  // = x-coordinate of upper left corner of square
+        int y = 1;  // = y-coordinate of upper left corner of square
+        int subW = 1;
+        int subH = 1;
+        int rW = 1;
+        int rH = 1;
 
-		} catch (MalformedURLException e) { e.printStackTrace(); }
-		catch (IOException ex) { ex.printStackTrace(); }
+        BufferedImage bimg = null;
+        URL url = null;
 
+        try {       //loads image during runtime - "loading" is printed in console to display status of image stream
 
-		//break up bimg into 9 [almost] equal parts
-		images[0][0] = bimg.getSubimage(x, y, subW, subH);
-		images[0][1] = bimg.getSubimage(subW, y, subW, subH);
-		images[0][2] = bimg.getSubimage(subW * 2, y, subW, subH);
-		images[1][0] = bimg.getSubimage(x, subH, subW, subH);
-		images[1][1] = bimg.getSubimage(subW, subH, subW, subH);
-		images[1][2] = bimg.getSubimage(subW * 2, subH, subW, subH);
-		images[2][0] = bimg.getSubimage(x, subH * 2, subW, subH);
-		images[2][1] = bimg.getSubimage(subW, subH * 2, subW, subH);
-		images[2][2] = bimg.getSubimage(subW * 2, subH * 2, subW, subH);
+            //url = new URL("http://www.papertraildesign.com/wp-content/uploads/2017/06/emoji-sunglasses.png");       //sunglasses smiley face
+            url = new URL("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWOi91m_B4JhbQnDbkX_iWd_YfuNK4NPHqxQYWdmbe09LiEIgG");    //beach ball
+            //url = new URL("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQz_NSSDOxhKFPS4kWTLZdkar2qY3KrZM9F9oZn6WhT2UzvXXLkJQ");    //puppies
+            //url = new URL("https://image.shutterstock.com/z/stock-vector-high-saturation-colored-hologram-sticker-326338961.jpg");    //rainbow metal
+            //url = new URL("https://cdn.pixabay.com/photo/2015/10/21/02/16/rainbow-background-998740_960_720.jpg");      //rainbow abstract
 
-		return images;
-	}
+            bimg =  ImageIO.read(url);
+            subW = ImageIO.read(url).getWidth() / 3;
+            rW = ImageIO.read(url).getWidth() % 3;
+            subH = ImageIO.read(url).getHeight() / 3;
+
+        } catch (MalformedURLException e) { e.printStackTrace(); }
+        catch (IOException ex) { ex.printStackTrace(); }
 
 
-	public ImageIcon getScaledImage(ImageIcon img) {        //scales subImage to equal size of JButton
-		ImageIcon scaledImage = img;
-		Image image = img.getImage(); // transform to img Image object
-		Image newImage = image.getScaledInstance(366, 366,  java.awt.Image.SCALE_SMOOTH); // scale to match size of JButton (Container height (1100) and width (1100) divided by 3 (=366x366))
-		scaledImage = new ImageIcon(newImage);  // transform back to ImageIcon object
-		return scaledImage;
-	}
 
-	public Point getIndex(JButton btn, JButton[][] board) { 		//returns the index of a given point
-		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 3; j++) {
-				if( board[i][j].equals(btn) ) {
-					return new Point(i, j);
-				}
-			}
-		}
-		return null;
-	}
+        //break up bimg into 9 [almost] equal parts
+        images[0][0] = bimg.getSubimage(x, y, subW, subH);
+        images[0][1] = bimg.getSubimage(subW, y, subW, subH);
+        images[0][2] = bimg.getSubimage(subW * 2 , y, subW, subH);
+        images[1][0] = bimg.getSubimage(x, subH, subW, subH);
+        images[1][1] = bimg.getSubimage(subW, subH, subW, subH);
+        images[1][2] = bimg.getSubimage(subW * 2, subH, subW, subH);
+        images[2][0] = bimg.getSubimage(x, subH * 2, subW, subH);
+        images[2][1] = bimg.getSubimage(subW, subH * 2, subW, subH);
+        images[2][2] = bimg.getSubimage(subW * 2, subH * 2, subW, subH);
+
+        return images;
+    }
 
 
-	public boolean checkNextTo(JButton btn, JButton nullBtn, JButton[][] board) {    //checks if the button clicked on is vertically or horizontally next to the null button
-		btnPoint = getIndex(btn,board);
-		nullBtnPoint = getIndex(nullBtn,board);
 
-		if ((btnPoint.equals(one)) && (nullBtnPoint.equals(two) || nullBtnPoint.equals(four))) {return true;}
-		if ((btnPoint.equals(two)) && (nullBtnPoint.equals(one) || nullBtnPoint.equals(three) || nullBtnPoint.equals(five))) {return true;}
-		if ((btnPoint.equals(three)) && (nullBtnPoint.equals(two) || nullBtnPoint.equals(six))) {return true;}
-		if ((btnPoint.equals(four)) && (nullBtnPoint.equals(one) || nullBtnPoint.equals(five) || nullBtnPoint.equals(seven))) {return true;}
-		if ((btnPoint.equals(five)) && (nullBtnPoint.equals(two) || nullBtnPoint.equals(four) || nullBtnPoint.equals(six) || nullBtnPoint.equals(eight))) {return true;}
-		if ((btnPoint.equals(six)) && (nullBtnPoint.equals(three) || nullBtnPoint.equals(five) || nullBtnPoint.equals(nine))) {return true;}
-		if ((btnPoint.equals(seven)) && (nullBtnPoint.equals(four) || nullBtnPoint.equals(eight))) {return true;}
-		if ((btnPoint.equals(eight)) && (nullBtnPoint.equals(five) || nullBtnPoint.equals(seven)|| nullBtnPoint.equals(nine))) {return true;}
-		if ((btnPoint.equals(nine)) && (nullBtnPoint.equals(six) || nullBtnPoint.equals(eight))) {return true;}
+    public ImageIcon getScaledImage(ImageIcon img) {        //scales subImage to equal size of JButton
+        ImageIcon scaledImage = img;
+        Image image = img.getImage(); // transform to img Image object
+        Image newImage = image.getScaledInstance(366, 366,  java.awt.Image.SCALE_SMOOTH); // scale to match size of JButton (Container height (1100) and width (1100) divided by 3 (=366x366))
+        scaledImage = new ImageIcon(newImage);  // transform back to ImageIcon object
+        return scaledImage;
+    }
 
-		else {return false;}
-	}
+    public Point getIndex(JButton btn, JButton[][] board) { 		//returns the index of a given point
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if( board[i][j].equals(btn) ) {
+                    return new Point(i, j);
+                }
+            }
+        }
+        return null;
+    }
 
-	public JButton getNullButton(JButton[][] board) {
-		JButton theNullBtn = new JButton();
 
-		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 3; j++) {
-				if (board[i][j].getText() == ("")) {
-					theNullBtn = board[i][j];
-				}
-			}
-		}
-		return theNullBtn;
-	}
+    public boolean checkNextTo(JButton btn, JButton nullBtn, JButton[][] board) {    //checks if the button clicked on is vertically or horizontally next to the null button
+        btnPoint = getIndex(btn,board);
+        nullBtnPoint = getIndex(nullBtn,board);
+
+        if ((btnPoint.equals(one)) && (nullBtnPoint.equals(two) || nullBtnPoint.equals(four))) {return true;}
+        if ((btnPoint.equals(two)) && (nullBtnPoint.equals(one) || nullBtnPoint.equals(three) || nullBtnPoint.equals(five))) {return true;}
+        if ((btnPoint.equals(three)) && (nullBtnPoint.equals(two) || nullBtnPoint.equals(six))) {return true;}
+        if ((btnPoint.equals(four)) && (nullBtnPoint.equals(one) || nullBtnPoint.equals(five) || nullBtnPoint.equals(seven))) {return true;}
+        if ((btnPoint.equals(five)) && (nullBtnPoint.equals(two) || nullBtnPoint.equals(four) || nullBtnPoint.equals(six) || nullBtnPoint.equals(eight))) {return true;}
+        if ((btnPoint.equals(six)) && (nullBtnPoint.equals(three) || nullBtnPoint.equals(five) || nullBtnPoint.equals(nine))) {return true;}
+        if ((btnPoint.equals(seven)) && (nullBtnPoint.equals(four) || nullBtnPoint.equals(eight))) {return true;}
+        if ((btnPoint.equals(eight)) && (nullBtnPoint.equals(five) || nullBtnPoint.equals(seven)|| nullBtnPoint.equals(nine))) {return true;}
+        if ((btnPoint.equals(nine)) && (nullBtnPoint.equals(six) || nullBtnPoint.equals(eight))) {return true;}
+
+        else {return false;}
+    }
+
+    public JButton getNullButton(JButton[][] board) {
+        JButton theNullBtn = new JButton();
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j].getText() == ("")) {
+                    theNullBtn = board[i][j];
+                }
+            }
+        }
+        return theNullBtn;
+    }
 }
